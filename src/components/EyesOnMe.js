7479cfe55a8e5f664b2c78b1a17e1c58c42d607d
Tommy Nguyen <sixty22tommy@gmail.com>
2022-07-20 23:25:32 -0700

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,26 @@
+import React from "react";
+// import Keypad from "./Keypad";
 // Code EyesOnMe Component Here
+
+function EyesOnMe (){
+    function focusEyes (event) {
+        event.preventDefault()
+        console.log('Good!')
+    }
+    function blurEyes (event) {
+        console.log('Hey! Eyes on me!')
+    }
+    return (
+        <div>
+            <button onFocus={focusEyes} onBlur={blurEyes} > 
+                'Good!'
+                'Hey! Eyes on me!'
+            </button>
+            
+        </div>
+    )
+}
+
+
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,18 @@
+import React from "react";
+// import EyesOnMe from "./EyesOnMe";
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleChange(event){
+        event.preventDefault()
+        console.log('Entering password...')
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handleChange}>
+            
+            </input>
+        </div>
     )
 }
 
